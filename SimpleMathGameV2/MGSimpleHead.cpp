f0d422a0542973906465838fde962dc6100bd54b
Jan Bielawa <Jan.bielawa@student.uj.edu.pl>
2022-03-23 13:26:21 +0100

    Simplified code

--- SimpleMathGameV2/MGSimpleHead.cpp
+++ SimpleMathGameV2/MGSimpleHead.cpp
@@ -9,21 +9,22 @@
 // In order: Addition, substraction, multipltion, devidition
 
 
-void Problem::CreateProblem(uint16_t lDifficult, uint16_t lAct)
+void Problem::CreateProblem()
 {
-	switch (lAct)
+	switch (lAction)
 	{
 	case 1:
 	{
-		nFirstI = rand() % (int)pow(10, lDifficult + 1) + 1;
-		nSecondI = rand() % (int)pow(10, lDifficult + 1) + 1;
+		nFirstI = rand() % (int)pow(10, lDif + 1) + 1;
+		nSecondI = rand() % (int)pow(10, lDif + 1) + 1;
 		break;
 	}
 	case 2:
 	{
 		do
 		{
-			nFirstI = rand() % (int)pow(10, lDifficult + 1) + 1;
+			nFirstI = rand() % (int)pow(10, lDif + 1) + 1;
+			nSecondI = rand() % (int)pow(10, lDif + 1) + 1;
 		} while (nSecondI > nFirstI);
 		break;
 	}
@@ -41,32 +42,36 @@ void Problem::CreateProblem(uint16_t lDifficult, uint16_t lAct)
 	
 }
 
-void Problem::Display()
+char Problem::cCheckActionSymbol()
 {
-	char cAct = ' ';
 	switch (lAction)
 	{
 	case 1:
 	{
-		cAct = '+';
+		return '+';
 		break;
 	}
 	case 2:
 	{
-		cAct = '-';
+		return '-';
 		break;
 	}
 	case 3:
 	{
-		cAct = '*';
+		return '*';
 		break;
 	}
 	case 4:
 	{
-		cAct = '/';
+		return '/';
 		break;
 	}
 	}
-	printf_s("%d %c %d = ", nFirstI, cAct, nSecondI);
+}
+
+void Problem::Display()
+{
+	CreateProblem();
+	printf_s("%d %c %d = ", nFirstI, cCheckActionSymbol(), nSecondI);
 }
 
--- SimpleMathGameV2/SimpleMathGameV2.cpp
+++ SimpleMathGameV2/SimpleMathGameV2.cpp
@@ -1,8 +1,11 @@
 ﻿#include<iostream>
+#include"MGSimpleHead.h"
 uint32_t score = 0;
 
 
 int main()
 {
 	srand(time(NULL));
+	Problem Test;
+	Test.Display();
 }
