2a3272550d1080d19fad77ea71629a59299cce68
Jan Bielawa <Jan.bielawa@student.uj.edu.pl>
2022-03-24 15:45:05 +0100

    Added functionality to switching the difficult level.

--- SimpleMathGameV2/SimpleMathGameV2.cpp
+++ SimpleMathGameV2/SimpleMathGameV2.cpp
@@ -4,6 +4,11 @@
 #include<ctime>
 #include<conio.h>
 #include"MGSimpleHead.h"
+
+// uint16_t lMaxDif = 5 
+// You can change the difficulty settings in code so you can have i.e. From 1 to 100.
+
+
 uint32_t score = 0;
 uint16_t lMainDifficult = 1;
 bool bIsGameOn = true;
@@ -17,9 +22,12 @@ int main()
 	Problem initialize;
 	do
 	{
+		score = 0;
 		chooseDif();
+		initialize.lDif = lMainDifficult;
 		problemDisplay(initialize);
 		_getch();
+		system("cls");
 	} while (bIsGameOn);
 }
 
@@ -34,7 +42,7 @@ void chooseDif()
 		printf("3. Advanced\n");
 		printf("4. Hard\n");
 		std::getline(std::cin, tmp);
-		if (tmp[0] < '1' || tmp[0] > '4' || tmp[0] == '\0')
+		if (tmp[0] < '1' || tmp[0] > '4' || tmp[0] == '\0') // TODO - Better check if answer is valid
 		{
 			printf("Enter a valid answer!");
 			_getch();
@@ -42,10 +50,19 @@ void chooseDif()
 		}
 		else
 		{
-			lMainDifficult = std::stoi(tmp);
+			if (std::stoi(tmp) > 5)
+			{
+				printf("Enter a valid answer!");
+				_getch();
+				system("cls");
+			}
+			else
+			{
+				lMainDifficult = std::stoi(tmp);
+			}
 			system("cls");
 		}
-	} while (tmp[0] < '1' || tmp[0] > '4' || tmp[0] == '\0');
+	} while (tmp[0] < '1' || tmp[0] > '4' || tmp[0] == '\0' || std::stoi(tmp) > 5); // You can replace std::stoi(tmp) > 5 with std::stoi(tmp) > lMaxDif
 
 }
 void problemDisplay(Problem Instance)
@@ -53,11 +70,8 @@ void problemDisplay(Problem Instance)
 	for (int i = 0; i < 5 * lMainDifficult; i++)
 	{
 		printf("%d Problem\n", i + 1);
-		//Instance.lAction = rand() % 4 + 1;
-		Instance.lAction = 4;
+		Instance.lAction = rand() % 4 + 1;
 		Instance.Display();
 		printf("\n");
-		//_getch();
-		//system("cls");
 	}
 }
